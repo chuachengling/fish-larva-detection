816ce252f22639d324e326f5e9bd451bcf2e9e2a
Bhavesh Chainani <bhaveshchainani@gmail.com>
2021-11-03 17:54:08 +0800

    Added download button for JSON file. Yet to test.

--- frontend/frontend_app.py
+++ frontend/frontend_app.py
@@ -1,6 +1,7 @@
 import streamlit as st 
 from utils import *
 from collections import Counter
+import matplotlib.pyplot as plt
 import glob
 import cv2
 import pandas as pd
@@ -45,9 +46,10 @@ st.markdown(
  
 ##backend will run the model
 
-st.title('Welcome to DSA4266 Group 1 Fish Larva Detection Application')
+st.markdown("<h1 style='text-align: center; color: white;'>DSA4266 Group 1</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white;'>Fish Larva Object Detection</h1>", unsafe_allow_html=True)
 
-file = st.file_uploader('Please upload an image',type = ['png','jpg','jpeg'])
+file = st.file_uploader('Please upload a test image:',type = ['png','jpg','jpeg'])
 
 def predict(url, file):
     #image = open(file, 'rb')
@@ -81,16 +83,35 @@ if file:
     #Showcase Inferenced Image
     st.write('### Inferenced Image')
 
+    ##code to display inferenced image here
+    
+    #Create DF to showcase class counts
+
     list_new = []
     for i in response:
         list_new.append(i["category_name"])
 
     count_model = Counter(list_new)
-    
-    #Create DF
     df = pd.DataFrame.from_dict(count_model, orient='index').reset_index()
     df = df.rename(columns={"index": "class", 0: "model_count"})
     st.dataframe(df)
+
+    ## Generate list of confidences.
+    confidences = [box['confidence'] for box in response['predictions']]
+
+    ## Summary statistics section in main app.
+    st.write('### Summary Statistics')
+    st.write(f'Number of Bounding Boxes (ignoring overlap thresholds): {len(confidences)}')
+    st.write(f'Average Confidence Level of Bounding Boxes: {(np.round(np.mean(confidences),4))}')
+
+    ## Histogram in main app.
+    st.write('### Histogram of Confidence Levels')
+    fig, ax = plt.subplots()
+    ax.hist(confidences, bins=10, range=(0.0,1.0))
+    st.pyplot(fig)
+
+    # Download JSON
+    st.download_button(label='Download JSON', data=response)
 
 
 
