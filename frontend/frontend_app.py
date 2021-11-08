c78926cc0d8fd3c4970c6bec7e9d23eed7be6926
chuachengling <chuachengling@u.nus.edu>
2021-11-09 00:01:00 +0800

    service

--- frontend/frontend_app.py
+++ frontend/frontend_app.py
@@ -80,7 +80,7 @@ def rename_class(row):
       return 'unidentifiable object'
 
 if file:
-    response = predict('http://0.0.0.0:80/api/',file)
+    response = predict('http://54.169.143.240:80/api/',file)
     print('JSON file has been received')
 
     response = json.loads(response.text)
